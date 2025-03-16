9485f4fb9deb47d5b5968b20d27dafdcdb4f15b6
Valentin Stanciu <stanciu327@gmail.com>
2025-03-16 21:25:20 +0000

    custom link prod isssue

--- components/custom-link.tsx
+++ components/custom-link.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 interface CustomLinkProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -14,9 +14,11 @@ export default function CustomLink({
   className,
   ...props
 }: CustomLinkProps) {
+  const router = useRouter();
+  
   return (
     <div
-      onMouseDown={() => redirect(href)}
+      onMouseDown={() => router.push(href)}
       className={cn("hover:cursor-pointer", className)}
       {...props}
     >
