ea99c6422afbe393ce6cbe27eb06f00a0ee737f1
Valentin Stanciu <stanciu327@gmail.com>
2025-03-21 10:12:35 +0000

    added new auth

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -4,5 +4,10 @@ export default {
       domain: "https://alert-kangaroo-21.clerk.accounts.dev",
       applicationID: "convex",
     },
+    // Production Clerk domain
+    {
+      domain: "https://clerk.sereni.day", // Add your production Clerk domain
+      applicationID: "convex",
+    },
   ]
 };
